d191550c91dae34f12557db4f7d778f6e98e88a3
Keoni D'Souza <keondsou@c02jf1nkdrvc.itvplc.ads>
2018-07-30 12:21:19 +0100

    Removed old commented-out code

--- src/App.js
+++ src/App.js
@@ -2,109 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 // import Select from 'react-select';
-// import './App.css';
-// import api from './categories.json';
-
-// const CATEGORIES = ["Children",
-//     "Comedy",
-//     "Drama & Soaps",
-//     "Entertainment",
-//     "Factual",
-//     "Films",
-//     "News",
-//     "Sport",
-// ]
-
-// export default class PersonList extends React.Component {
-//     state = {
-//         // persons : []
-//         // name: "",
-//         id: "",
-//     }
-//
-//     // componentDidMount() {
-//     //     axios.get("https://jsonplaceholder.typicode.com/users")
-//     //         .then(res => {
-//     //             const persons = res.data;
-//     //             this.setState({ persons })
-//     //         })
-//     // }
-//
-//     handleChange = event => {
-//         // this.setState({name: event.target.value});
-//         this.setState({ id: event.target.value });
-//     }
-//
-//     handleSubmit = event => {
-//         event.preventDefault();
-//
-//         // const user = {
-//         //     name: this.state.name
-//         // };
-//         //
-//         // axios.post(`https://jsonplaceholder.typicode.com/users`, {user})
-//         //     .then(res => {
-//         //         console.log(res);
-//         //         console.log(res.data);
-//         //     })
-//         axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
-//             .then(res => {
-//                 console.log(res);
-//                 console.log(res.data);
-//             })
-//     }
-//
-//     render() {
-//         // return <ul>
-//         //     {this.state.persons.map(
-//         //         person => <li>{person.name}</li>
-//         //     )}
-//         // </ul>
-//         // return (
-//         //     <div>
-//         //         <form onSubmit={this.handleSubmit}>
-//         //             <label>
-//         //                 Person Name:
-//         //                 <input type="text" name="name" onChange={this.handleChange} />
-//         //             </label>
-//         //             <button type="submit">Add</button>
-//         //         </form>
-//         //     </div>
-//         // )
-//         return (
-//             <div>
-//                 <form onSubmit={this.handleSubmit}>
-//                     <label>
-//                         Person ID:
-//                         <input type="text" name="id" onChange={this.handleChange} />
-//                     </label>
-//                     <button type="submit">Delete</button>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
-
-// export default class App extends React.Component {
-//
-//     render() {
-//         CATEGORIES.forEach(function (cat) {
-//             return cat;
-//         })
-//
-//         return (
-//             <div>
-//                 <h1>Categories</h1>
-//                 <ul>
-//                     {CATEGORIES.map(
-//                         cat => <li key={(cat.substring(0,3))}>{cat}</li>
-//                     )}
-//                 </ul>
-//             </div>
-//         );
-//     }
-//
-// }
 
 export default class App extends Component {
     constructor(props) {
@@ -113,7 +10,7 @@ export default class App extends Component {
             categories: [],
             programmes: [],
             category: ''
-        }
+        };
         this.handleClick = this.handleClick.bind(this);
     }
 
@@ -166,7 +63,7 @@ export default class App extends Component {
 
     handleClick(event) {
         event.preventDefault();
-        this.getProgrammes(event.target.innerHTML)
+        this.getProgrammes(event.target.innerHTML);
         this.setState({category: event.target.innerHTML});
 
     }
@@ -180,7 +77,7 @@ export default class App extends Component {
                 <ul>
                     {this.state.categories.map(category => {
                         return (
-                            <a href='#' key={category.name}>
+                            <a href='#' key={category.name.substring(0,3)}>
                                 <li onClick={this.handleClick}>{category.name}</li>
                             </a>
                         )
