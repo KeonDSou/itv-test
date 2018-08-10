76b36c7b01bfead038fa306ce68416e0c00818c0
KeonDSou <keoni.dsouza@itv.com>
2018-08-10 12:07:52 +0100

    KITV-4 - Date reformatted.

--- src/App.js
+++ src/App.js
@@ -160,12 +160,79 @@ export default class App extends Component {
             //     // Removes seconds from time
             //         .replace(':00', '').toLowerCase();
 
+            // const formatDate =
+            //     broadcastDate.toLocaleTimeString()
+            //     // Removes seconds from time
+            //         .replace(':00', '').toLowerCase()
+            //     + ' '
+            //     + broadcastDate.toLocaleDateString('en-gb');
+
+        const dayPart = () => {
+            switch (broadcastDate.getUTCDay()) {
+                case 0 :
+                    return 'Sunday';
+                case 1 :
+                    return 'Monday';
+                case 2 :
+                    return 'Tuesday';
+                case 3 :
+                    return 'Wednesday';
+                case 4 :
+                    return 'Thursday';
+                case 5 :
+                    return 'Friday';
+                case 6 :
+                    return 'Saturday';
+            }
+        };
+
+        const monthPart = () => {
+            switch (broadcastDate.getUTCMonth()) {
+                case 0 :
+                    return 'January';
+                case 1 :
+                    return 'February';
+                case 2 :
+                    return 'March';
+                case 3 :
+                    return 'April';
+                case 4 :
+                    return 'May';
+                case 5 :
+                    return 'June';
+                case 6 :
+                    return 'July';
+                case 7 :
+                    return 'August';
+                case 8 :
+                    return 'September';
+                case 9 :
+                    return 'October';
+                case 10 :
+                    return 'November';
+                case 11 :
+                    return 'December';
+            }
+        };
+
+        // const formatDatePart = () => {
+        //     return dayPart()
+        //         + broadcastDate.getUTCDate()
+        //         + monthPart();
+        // };
+
         const formatDate =
             broadcastDate.toLocaleTimeString()
             // Removes seconds from time
                 .replace(':00', '').toLowerCase()
             + ' '
-            + broadcastDate.toLocaleDateString('en-gb');
+            // + broadcastDate.toLocaleDateString('en-gb')
+            // + formatDatePart()
+            + dayPart()
+            + ' '
+            + broadcastDate.getUTCDate()
+            + ' '
+            + monthPart();
 
         /**
          * Calculates how many days are left to watch an episode
