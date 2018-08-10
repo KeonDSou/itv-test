cc907bc76db3f50cb23f8b0215610549c3a93e26
KeonDSou <keoni.dsouza@itv.com>
2018-08-10 14:21:24 +0100

    KITV-5 - All episodes now display channel of last broadcast.

--- src/components/programmes-display.js
+++ src/components/programmes-display.js
@@ -11,34 +11,40 @@
 
 import React from 'react';
 
-/**
- * TODO Add programme channel logo
- */
-
 const ProgrammesDisplay = (
     {programmes, handleClick}
-    ) => {
-    return <div className={'row'}>
-        {programmes.map(programme => {
-            return <div
-                onClick={handleClick}
-                id={'programme-card'}
-                className={'col-lg-4 col-lg-6 '}
-                key={programme.title}>
-                <h3
-                    className='title'
-                >{programme.title}</h3>
-                <img
-                    className={programme.title}
-                    src={programme._embedded.latestProduction._links.image.href}
-                    alt={programme.title}
-                />
-                <p
-                    className={programme.title}
-                >{programme.synopses.ninety}</p>
-            </div>
-        })}
-    </div>
+) => {
+    return (
+        <div className={'row'}>
+            {programmes.map(programme => {
+                return <div
+                    onClick={handleClick}
+                    id={'programme-card'}
+                    className={'col-lg-4 col-lg-6 '}
+                    key={programme.title}>
+
+                    <div className='row header'>
+                        <h3 className='col-sm-10 prog-title'>
+                            {programme.title}
+                        </h3>
+                        <img className={'col-sm-2 channel'}
+                             src={programme._embedded.latestProduction._embedded.channel._links.primaryImage.href}
+                             alt={`${programme._embedded.latestProduction._embedded.channel.name} logo`}
+                        />
+                    </div>
+
+                    <img
+                        className={programme.title}
+                        src={programme._embedded.latestProduction._links.image.href}
+                        alt={programme.title}
+                    />
+                    <p
+                        className={programme.title}
+                    >{programme.synopses.ninety}</p>
+                </div>
+            })}
+        </div>
+    )
 };
 
 export default ProgrammesDisplay;
