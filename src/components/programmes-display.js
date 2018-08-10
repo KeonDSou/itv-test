7164e8ab687bc5af931917a56f5818638d5ecf2c
KeonDSou <keoni.dsouza@itv.com>
2018-08-10 13:42:08 +0100

    KITV-5 - Removed unnecessary else from SingleEpisodeDisplay.

--- src/components/programmes-display.js
+++ src/components/programmes-display.js
@@ -18,29 +18,27 @@ import React from 'react';
 const ProgrammesDisplay = (
     {programmes, handleClick}
     ) => {
-    return (
-        <div className={'row'}>
-            {programmes.map(programme => {
-                return <div
-                    onClick={handleClick}
-                    id={'programme-card'}
-                    className={'col-lg-4 col-lg-6 '}
-                    key={programme.title}>
-                    <h3
-                        className='title'
-                    >{programme.title}</h3>
-                    <img
-                        className={programme.title}
-                        src={programme._embedded.latestProduction._links.image.href}
-                        alt={programme.title}
-                    />
-                    <p
-                        className={programme.title}
-                    >{programme.synopses.ninety}</p>
-                </div>
-            })}
-        </div>
-    )
+    return <div className={'row'}>
+        {programmes.map(programme => {
+            return <div
+                onClick={handleClick}
+                id={'programme-card'}
+                className={'col-lg-4 col-lg-6 '}
+                key={programme.title}>
+                <h3
+                    className='title'
+                >{programme.title}</h3>
+                <img
+                    className={programme.title}
+                    src={programme._embedded.latestProduction._links.image.href}
+                    alt={programme.title}
+                />
+                <p
+                    className={programme.title}
+                >{programme.synopses.ninety}</p>
+            </div>
+        })}
+    </div>
 };
 
 export default ProgrammesDisplay;
